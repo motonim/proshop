02316e89cb689a7dd60405ff9d90da54f426b68f
Jaeri Park <motonimjrp@gmail.com>
2023-06-11 16:36:26 -0400

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+// 보통은 const express = require('express') 지만 frontend import 과 통일성 있게 쓰기 위해서 package.json 에서   "type": "module"로 명시하고 import 사용가능. otherwise, by default, it's type: commonjs where we can only use const, require...
 import dotenv from 'dotenv'
 dotenv.config()
-// 보통은 const express = require('express') 지만 frontend import 과 통일성 있게 쓰기 위해서 package.json 에서   "type": "module"로 명시하고 import 사용가능. otherwise, by default, it's type: commonjs where we can only use const, require...
+import connectDB from './config/db.js'
 import products from './data/products.js'
-const port = process.env.PORT || 5000; // if env.port doesn't exist, try with port 5000
+const port = process.env.PORT || 5000 // if env.port doesn't exist, try with port 5000
+
+connectDB() // Connect to MongoDB
 
 const app = express()
 
