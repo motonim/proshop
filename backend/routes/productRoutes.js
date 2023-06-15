c306a4f76cd4aa5c77d131ad3fdbf678d7538945
Jaeri Park <jaeripark@Jaeris-MacBook-Pro-2.local>
2023-06-15 14:22:48 -0400

    fetch products from database using mongoose

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,22 @@
 import express from "express"
 const router = express.Router()
-import products from '../data/products.js'
+// import products from '../data/products.js'
+import asyncHandler from "../middleware/asyncHandler.js"
+import Product from '../models/productModel.js'
 
-router.get('/', async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({}) // reason why empty bracket - to fetch all data
     res.json(products)
-  })
+}))
   
-router.get('/:id', (req, res) => {
+router.get('/:id', asyncHandler (async (req, res) => {
     // :id is a placeholder
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+    const product = await Product.findById(req.params.id)
+    if(product) {
+       return res.json(product)
+    }
+
+    res.status(404).json({ message: 'Product not found'})
+}))
 
 export default router
