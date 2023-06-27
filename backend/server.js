1dd85a1b7ccbf0d46a0e843d31b70c42039ca63d
Jaeri Park <motonimjrp@gmail.com>
2023-06-26 23:35:15 -0400

    set user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config()
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 const port = process.env.PORT || 5000 // if env.port doesn't exist, try with port 5000
 
 connectDB() // Connect to MongoDB
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes) // everytime we use /api/products, it's gonna call productRoutes
+app.use('/api/users', userRoutes) // everytime we use /api/users, it's gonna call userRoutes
 
 app.use(notFound)
 app.use(errorHandler)
